27e6132c916cb2f84afac001b6cffb5c7785ed0f
Maria Leitão <malufln.99@gmail.com>
2024-07-11 17:34:55 -0300

    feat: implement real-time text synchronization across clients with Socket.io

--- public/document.js
+++ public/document.js
@@ -5,3 +5,7 @@ const textEditor = document.getElementById("text-editor");
 textEditor.addEventListener("keyup", () => {
   socket.emit("text-editor", textEditor.value);
 });
+
+socket.on("text-editor-clients", (text) => {
+  textEditor.value = text;
+});
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,8 +2,8 @@ import io from "./server.js";
 
 io.on("connection", (socket) => {
   console.log("A connection was detected. Client ID:", socket.id);
-  
+
   socket.on("text-editor", (text) => {
-    console.log(text);
+    socket.broadcast.emit("text-editor-clients", text);
   });
 });
