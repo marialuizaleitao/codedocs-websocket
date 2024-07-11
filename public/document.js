ba561d7c40a5976c2dfb0ba805d45079a290d01b
Maria Leitão <malufln.99@gmail.com>
2024-07-11 17:31:12 -0300

    feat: add real-time text editor synchronization with Socket.io

--- public/document.js
+++ public/document.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const textEditor = document.getElementById("text-editor");
+
+textEditor.addEventListener("keyup", () => {
+  socket.emit("text-editor", textEditor.value);
+});
