e060d014d53bfde8ca71e73ee1c6aadb741b2545
Maria Leitão <malufln.99@gmail.com>
2024-07-11 17:21:52 -0300

    feat: socket.io initial implementation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = process.env.port || 3000;
@@ -10,6 +12,14 @@ const publicDir = path.join(currentPath, "../..", "public");
 
 app.use(express.static(publicDir));
 
-app.listen(PORT, () => {
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("A connection was detected.")
+})
