4fb67d6b7c5d6d52f4b2caeb09120c666177bcf8
Maria Leitão <malufln.99@gmail.com>
2024-07-12 13:10:20 -0300

    feat: integrate document selection with locally stored documents for text editor

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,28 @@
 import io from "./server.js";
 
+const documents = [
+  {
+    name: "JavaScript",
+    text: "JavaScript code...",
+  },
+  {
+    name: "Node",
+    text: "Node.js code...",
+  },
+  {
+    name: "Socket.io",
+    text: "Socket.io code...",
+  },
+];
+
 io.on("connection", (socket) => {
   console.log("A connection was detected. Client ID:", socket.id);
 
   socket.on("select-document", (name) => {
+    const document = findDocument(name);
+
+    console.log(document);
+
     socket.join(name);
   });
 
@@ -11,3 +30,10 @@ io.on("connection", (socket) => {
     socket.to(documentName).emit("text-editor-clients", text);
   });
 });
+
+function findDocument(name) {
+  const document = documents.find((document) => {
+    return document.name === name;
+  });
+  return document;
+}
